b18961f681953f99196574d66bd592fd7ff179dd
Sameer-Sarrainodu <sameer339977.com>
2026-05-21 15:03:09 +0530

    updated

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -6,6 +6,22 @@ echo "🚀 Installing Ansible collections..."
 
 ansible-galaxy collection install -r collections/requirements.yml
 
-echo "🚀 Starting ecommerce deployment..."
+echo "🚀 Configuring DB and Middleware Servers..."
 
-ansible-playbook playbooks/site.yml
+ansible-playbook \
+  -i inventories/dev/hosts.ini \
+  playbooks/site.yml
+
+echo "🚀 Initializing Packer..."
+
+cd packer
+
+packer init .
+
+echo "🚀 Building Catalogue Image..."
+
+packer build \
+  -var-file=variables.pkrvars.hcl \
+  catalogue.pkr.hcl
+
+echo "✅ Catalogue image build completed successfully!"
